12b9a51b6510122e68213aa112e7fd2976445ad4
Joel Gallant <joelgallant236@gmail.com>
2015-02-10 11:50:10 -0700

    PowerUtils static & new voltagePID simple wrapper

---
+++ src/org/calgarycommteams/module/VoltagePID.java
@@ -0,0 +1,22 @@
+package org.calgarycommteams.module;
+
+import edu.first.identifiers.Input;
+import edu.first.module.actuators.SpeedController;
+import edu.first.module.controllers.PIDController;
+import edu.first.util.PowerUtils;
+
+/**
+ *
+ * @author Joel Gallant
+ */
+public class VoltagePID extends PIDController {
+
+    public VoltagePID(final int pdpPort, SpeedController output, double P) {
+        super(new Input() {
+            @Override
+            public double get() {
+                return PowerUtils.getCurrent(pdpPort);
+            }
+        }, output, P, 0, 0);
+    }
+}
