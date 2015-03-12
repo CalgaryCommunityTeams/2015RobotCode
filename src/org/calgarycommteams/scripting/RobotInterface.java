371ff9d0b24986edc7b6ee6db056fa388fff7222
Giantblargg <ddoran3@gmail.com>
2015-03-12 17:14:17 -0600

    added scripting framework

---
+++ src/org/calgarycommteams/scripting/RobotInterface.java
@@ -0,0 +1,17 @@
+package org.calgarycommteams.scripting;
+
+/**
+ * functions used to interface {@code Scripting} with the rest of the robot.
+ *
+ * @since March 12 15
+ * @author Daniel Doran
+ */
+class RobotInterface {
+	
+	RobotInterface() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//TODO Function called by the script to interact with the robot
+
+}
