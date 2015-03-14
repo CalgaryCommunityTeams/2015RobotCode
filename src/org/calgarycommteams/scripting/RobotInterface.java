3dfcd99accb20e829eb355308e14d7c8ff315d22
Giantblargg <ddoran3@gmail.com>
2015-03-14 03:25:33 -0600

    added sleep function for javascript

--- src/org/calgarycommteams/scripting/RobotInterface.java
+++ src/org/calgarycommteams/scripting/RobotInterface.java
@@ -1,17 +1,27 @@
 package org.calgarycommteams.scripting;
 
+
 /**
- * functions used to interface {@code Scripting} with the rest of the robot.
+ * Functions used to interface {@code Scripting} with the rest of the robot.
+ * Functions will be under robot.*
  *
  * @since March 12 15
  * @author Daniel Doran
  */
-class RobotInterface {
+public class RobotInterface {
 	
 	RobotInterface() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
-	//TODO Function called by the script to interact with the robot
+	public void sleep(long time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//TODO Functions called by the script to interact with the robot
 
 }
